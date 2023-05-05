65db3197da625c569ebf9ab327a5e107c7dcac4e
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-05 20:38:28 +0000

    set up folder structure and basic Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact';
+import Root from './components/Root';
 
-function App() {
+const router = createBrowserRouter( createRoutesFromElements (
+  <Route path='/' element={ <Root/> }>
+    <Route path='home' element={ <Home /> } />
+    <Route path='about' element={ <About /> } />
+    <Route path='work' element={ <Work /> } />
+    <Route path='contact' element={ <Contact /> } />  
+  </Route>
+));
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={ router } />
   );
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+export default function Header () {
+    return (
+        <div className="header">
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/work">Work</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+        </div>
+  )
+}
