6506ad40fde131bfd3ff8b2ec10dbed0cf111bc9
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-10 22:49:21 +0000

    about

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,23 @@ import React from "react";
 
 export default function About() {
   return (
-    <main>
+    <main id="aboutText">
+      <h2>Hi! My name is Stephanie.</h2>
+      <h2>Nice to meet you.</h2>
+      <br />
       <p>
-      As a self-taught Web Developer, I am passionate about building dynamic and engaging web experiences. 
+      A self-taught Web Developer, passionate about building dynamic and engaging web experiences. 
       My skills include HTML, CSS, JavaScript, and the ReactJS Framework.
+      </p>
+      <br />
+      <p>
       I am a quick learner and constantly expanding my knowledge to stay current with the latest trends and 
       technologies in web development. My attention to detail and problem-solving skills allow me to create 
-      clean and efficient code that enhances the user experience. With a strong focus on collaboration and 
+      clean and efficient code that enhances the user experience. 
+      </p>
+      <br />
+      <p>
+      With a strong focus on collaboration and 
       communication, I am always looking for new opportunities to work with others to create innovative 
       solutions.
       </p>
