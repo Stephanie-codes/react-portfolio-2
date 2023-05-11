d2882f25210558d889d02fa5456a794111963904
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-11 17:54:49 +0000

    resize imgs in work

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,48 +3,69 @@ import project1Img from "../images/reactquizapp.jpeg";
 import project2Img from "../images/todolist.jpeg";
 import project3Img from "../images/shop.jpeg";
 import project4Img from "../images/wordsearch.jpeg";
+import project5Img from "../images/portfoliogif.gif";
 
 const projects = [
 {
 name: "React Quiz App",
 imageSrc: project1Img,
-description: "My First Project using React!",
-link: "https://snazzy-choux-1960ae.netlify.app"
+description: "My first project using React!",
+link: "https://snazzy-choux-1960ae.netlify.app",
+gitlink: "https://github.com/Stephanie-codes/React-Quiz-App-New.git"
 },
 {
 name: "To Do List",
 imageSrc: project2Img,
-description: "This is my second project",
-link: "https://storied-phoenix-58c567.netlify.app"
+description: "HTML, CSS, JavaScript",
+link: "https://storied-phoenix-58c567.netlify.app",
+gitlink: "https://github.com/Stephanie-codes/To-Do-List.git"
 },
 {
   name: "Online Shop",
   imageSrc: project3Img,
-  description: "This is my second project",
-  link: "https://moonlit-axolotl-6a09fc.netlify.app/"
+  description: "HTML, CSS, JavaScript",
+  link: "https://moonlit-axolotl-6a09fc.netlify.app/",
+  gitlink: "https://github.com/Stephanie-codes/Online-Shop.git"
   },
   {
     name: "Wordsearch Generator",
     imageSrc: project4Img,
-    description: "This is my second project",
-    link: "https://strong-hamster-3b1367.netlify.app/"
-    }
+    description: "HTML, CSS, JavaScript",
+    link: "https://strong-hamster-3b1367.netlify.app/",
+    gitlink: "https://github.com/Stephanie-codes/WordSearch-Generator.git"
+    },
+    {
+      name: "Previous Portfolio",
+      imageSrc: project5Img,
+      description: "HTML, CSS, JavaScript & NodeJS for the Contact Form",
+      link: "https://preeminent-sorbet-ed558c.netlify.app/",
+      gitlink: "https://github.com/Stephanie-codes/Portfolio.git"
+      }
 ];
 
 export default function Work() {
-return (
-<main id="work">
-<h1 id="mywork">My work!</h1>
-<div id="workcontainer">
-  {projects.map((project, index) => (
-    <div key={index} className="project">
-    <img src={project.imageSrc} alt={project.name} />
-    <h2>{project.name}</h2>
-    <p>{project.description}</p>
-    <button href={project.link}>View project</button>
-    </div>
-  ))}
-</div>
-</main>
-);
+  const handleButtonClick = (url) => {
+    window.open(url, '_blank');
+  };
+
+  return (
+  <main id="work">
+  <h1 id="mywork">My work!</h1>
+  <div id="workcontainer">
+    {projects.map((project, index) => (
+      <div key={index} className="project">
+      <img src={project.imageSrc} alt={project.name} />
+      <h2>{project.name}</h2>
+      <p>{project.description}</p>
+      <button onClick={() => handleButtonClick(project.link)}>
+        View Website
+      </button>
+      <button onClick={() => handleButtonClick(project.gitlink)}>
+        View Github
+      </button>
+      </div>
+    ))}
+  </div>
+  </main>
+  );
 }
