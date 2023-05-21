3a561f08801787f48334238916e5aace25f9ad05
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-21 15:28:59 +0000

    add contact form, install express and nodemon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,17 @@ export default function Contact() {
   return (
     <main id="contact">
       <h1 id="contactme">Contact me!</h1>
+      <div class="form-container">
+        <form class="contact-form" id="contactform">
+            <div class="row">
+              <div class="boxes"><input type="text" id="fullname" name="name" placeholder="Full Name" /></div>
+              <div class="boxes"><input type="text" id="email" name="email" placeholder="Email" /></div>
+              <div class="boxes"><input type="text" id="subject" name="subject" placeholder="Subject" /></div>
+              <div class="boxes"><textarea id="message" cols="30" rows="10" name="message" placeholder="Message"></textarea></div>
+              <div><input id="submit" type="submit" class="submit" value="Send Message" /></div>
+            </div>
+        </form>
+      </div>
       <p>
         E: stephanie.burlinchon@gmail.com 
       </p>
