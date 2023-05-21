c7492bcf2678663f36efac77eec66be43cb6a5a2
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-21 15:48:25 +0000

    nodejs contact form

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 8080;
+
+// Middleware 
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'stephaniecodes202@gmail.com',
+            pass: 'zqfhxtlngqfmobsc'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'stephanieb170@gmail.com',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: `Message: ${req.body.message} from: ${req.body.fullName}`
+    }
+
+    try {
+        transporter.sendMail(mailOptions, (error, info) => {
+          if(error) {
+            console.log(error);
+            res.send('error');
+          } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+          }
+        });
+      } catch (err) {
+        console.log(err);
+        res.send('error');
+      }
+      
+
+  /*  transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log('error:' + error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+        }
+    })
+
+    app.get('/success', (req, res) => {
+
+        res.send('<h1>Your Message was Successfully Send!</h1>');
+    })
+    */
+    
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port: ${PORT}`)
+});
