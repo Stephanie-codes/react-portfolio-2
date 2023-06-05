912ab9ac58fc06640e5aafbcbc79faffe37514e9
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-06-05 20:35:17 +0000

    proxy

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(cors({ origin: 'https://incandescent-semifreddo-cadc54.netlify.app' }));
+app.use(cors());
+//app.use(cors({ origin: 'https://incandescent-semifreddo-cadc54.netlify.app' }));
 app.use(express.json({ limit: "25mb"}));
 app.use(express.urlencoded({ limit: "25mb" }));
 app.use((req, res, next) => {
